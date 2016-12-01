fbd2f2394266a205cf269403c6eedb0354964b2f
Nate Beaty <nate@clixel.com>
2016-12-01 11:57:27 -0600

    larger threshold for lazyload

--- public/assets/js/application.js
+++ public/assets/js/application.js
@@ -42,6 +42,7 @@ var Nb = (function($) {
 
     // Trigger lazyload
     lazyloader = new LazyLoad({
+      threshold: 500,
       callback_load: function(el) {
         // Add class to wrap to remove loading display
         $(el).parents('.ratiowrap:first').addClass('loaded');
