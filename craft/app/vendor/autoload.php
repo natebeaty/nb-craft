7dd903a89c771b0199fa965e5a07216ef0c35de6
Nate Beaty <nate@clixel.com>
2017-02-18 11:12:14 -0600

    update craft

--- craft/app/vendor/autoload.php
+++ craft/app/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit8ed5427131b11f61e2f91462686214d3::getLoader();
+return ComposerAutoloaderInit84303a2420d75ccfad0960bbc1e4edef::getLoader();
--- craft/app/vendor/composer/autoload_static.php
+++ craft/app/vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit8ed5427131b11f61e2f91462686214d3
+class ComposerStaticInit84303a2420d75ccfad0960bbc1e4edef
 {
     public static $prefixLengthsPsr4 = array (
         'l' => 
@@ -668,10 +668,10 @@ class ComposerStaticInit8ed5427131b11f61e2f91462686214d3
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit8ed5427131b11f61e2f91462686214d3::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit8ed5427131b11f61e2f91462686214d3::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit8ed5427131b11f61e2f91462686214d3::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInit8ed5427131b11f61e2f91462686214d3::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit84303a2420d75ccfad0960bbc1e4edef::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit84303a2420d75ccfad0960bbc1e4edef::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit84303a2420d75ccfad0960bbc1e4edef::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit84303a2420d75ccfad0960bbc1e4edef::$classMap;
 
         }, null, ClassLoader::class);
     }
