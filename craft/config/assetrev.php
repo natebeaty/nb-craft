e564efef9939211e3ac7567c9edb047b11bf3c31
nate <nate@clixel.com>
2017-05-07 03:04:03 +0000

    remove keycdn

--- craft/config/assetrev.php
+++ craft/config/assetrev.php
@@ -2,6 +2,6 @@
 return array(
 
   'manifestPath' => '../public/assets/dist/rev-manifest.json',
-  'assetUrlPrefix' => 'https://natebeaty-64c9.kxcdn.com/assets/dist/gz/',
+  'assetUrlPrefix' => '/assets/dist/gz/',
 
 );
--- public/index.php
+++ public/index.php
@@ -16,4 +16,4 @@ if (!is_file($path))
 	exit('Could not find your craft/ folder. Please ensure that <strong><code>$craftPath</code></strong> is set correctly in '.__FILE__);
 }
 
-/*HTMLCache Begin*/if (defined('CRAFT_PLUGINS_PATH')) {require_once CRAFT_PLUGINS_PATH . DIRECTORY_SEPARATOR . 'htmlcache' . DIRECTORY_SEPARATOR . 'functions' . DIRECTORY_SEPARATOR . 'htmlcache.php';} else {require_once str_replace('index.php', '../plugins' . DIRECTORY_SEPARATOR . 'htmlcache' . DIRECTORY_SEPARATOR . 'functions' . DIRECTORY_SEPARATOR . 'htmlcache.php', $path);}htmlcache_checkCache();/*HTMLCache End*/require_once $path;
+require_once $path;
