df2b3dabb41f59d8caa020489bda42bc1ba5efd5
Nate Beaty <nate@clixel.com>
2025-03-10 22:01:58 -0400

    more type tweaks, update fabfile to rsync assets

--- fabfile.py
+++ fabfile.py
@@ -1,39 +1,53 @@
 from fabric import task
 from invoke import run as local
+from patchwork.transfers import rsync
 
+# linode
 remote_path = "/home/natebeaty/apps/nb-craft"
-remote_hosts = ["natebeaty@natebeaty.opalstacked.com"]
-php_command = "php82"
-
-# set to production
-# @task
-# def production(c):
-#     global remote_hosts, remote_path
-#     remote_hosts = ["natebeaty@natebeaty.com"]
-#     remote_path = "/home/natebeaty/apps/nb-craft3-production"
+git_branch = "main"
+composer_command = "~/bin/composer.phar"
 
 # deploy
-@task(hosts=remote_hosts)
-def deploy(c):
+@task(optional=['assets'])
+def deploy(c, assets=None):
     update(c)
-    composer_install(c)
-    clear_cache(c)
+    composer_update(c)
+    if assets:
+        build_assets(c)
 
 def update(c):
-    c.run("cd {} && git pull".format(remote_path))
+    c.run("cd {} && git pull origin {}".format(remote_path, git_branch))
 
-def composer_install(c):
-    c.run("cd {} && {} ~/bin/composer.phar install".format(remote_path, php_command))
+def composer_update(c):
+    c.run("cd {} && {} install".format(remote_path, composer_command))
 
-def clear_cache(c):
-    c.run("cd {} && {} ./craft clear-caches/compiled-templates".format(remote_path, php_command))
-    c.run("cd {} && {} ./craft clear-caches/data".format(remote_path, php_command))
+def build_assets(c):
+    local("rm -rf assets/dist")
+    local("bun run gulp --production")
+    c.run("mkdir -p {}/web/assets/dist".format(remote_path))
+    rsync(c, "assets/dist", "{}/web/assets/".format(remote_path))
+    local("bun run gulp")
+
+@task()
+def clearcache(c):
+  c.run("cd {} && {} ./craft clear-caches/compiled-templates".format(remote_path, php_command))
+  c.run("cd {} && {} ./craft clear-caches/data".format(remote_path, php_command))
 
 # local commands
 @task
 def assets(c):
-    local("npx gulp --production")
+    local("bun run gulp --production")
 
 @task
 def dev(c):
-    local("npx gulp watch")
+    local("bun run gulp watch")
+
+# @task(optional=['syncdb'])
+# def syncstaging(c, syncdb=None):
+#     # `fab syncstaging --syncdb` to also restore latest database backup to cosm_staging
+#     if syncdb:
+#         print('Restoring latest backup to staging...')
+#         c.run('/usr/bin/zcat /home/natebeaty/backup/db/`ls -t /home/natebeaty/backup/db/ | head -n1` | /usr/bin/mysql --defaults-extra-file=/home/natebeaty/backup/.my.cnf --defaults-group-suffix=staging cosm_staging')
+#     print('Syncing product images...')
+#     c.run('/usr/bin/rsync -av --delete --exclude "orig" /var/www/microcosmpublishing.com/public_html/printpreviews/ /var/www/dev.microcosmpublishing.com/public_html/printpreviews/')
+#     c.run('/usr/bin/rsync -av --delete --exclude "orig" /var/www/microcosmpublishing.com/public_html/previews/ /var/www/dev.microcosmpublishing.com/public_html/previews/')
